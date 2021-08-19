fc5c3718244868202e94f9445f97ee02d25c565a
icaroaf <caroassisfreitas@globo.com>
2021-08-19 19:13:25 -0300

    cria middleware para verificar autenticação do usuário

---
+++ src/middlewares/loginVerifier.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const secret = require('../secret');
+const connection = require('../connection');
+
+const loginVerify = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(404).json("Token não informado");
+    }
+
+    try {
+        const token = authorization.replace('Bearer', '').trim();
+
+        const { cnpj } = jwt.verify(token, secret);
+
+        const query = 'SELECT * FROM empresas WHERE cnpj = ?';
+        const companyData = await conexao.query(query, [cnpj]);
+
+        if (!companyData) {
+            return res.status(404).json("A empresa não foi encontrada.");
+        }
+
+        const { senha, ...company } = companyData[0][0];
+
+        req.company = company;
+        next();
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+}
+
+module.exports = loginVerify;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const companies = require('./controllers/companies');
 const login = require('./controllers/login');
+const loginVerify = require('./middlewares/loginVerifier');
 
 const routes = express();
 
@@ -10,5 +11,7 @@ routes.post('/empresas', companies.signUpUser);
 
 //rota para login do usuário(empresa)
 routes.post('/login', login.login);
+
+routes.use(loginVerify);
 
 module.exports = routes;
